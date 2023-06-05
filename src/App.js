0a4f5e0031e033a6f7e7038ad0ccc4fa29c684ba
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2023-06-05 05:02:11 +0300

    add Routes and LanguageSwitcher component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
-
+import { Routes, Route } from "react-router-dom"
+import Home from './components/HomePage/Home';
+import About from './components/AboutPage/About';
+import LanguageSwitcher from './components/LanguageSwitcher';
 function App() {
   return (
     <div className="App">
-      hello khisnasmay
+      <h1 >Hello <span style={{ color: "#3485ff" }}>khisnasamy</span></h1>
+      <LanguageSwitcher />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path='/About' element={<About />} />
+      </Routes>
     </div>
   );
 }
