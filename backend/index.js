24320ff8b3e3c120da045f278dd6b529af9d7ef3
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2023-06-08 00:19:37 +0300

    check if connected with database and correct spelling in model

--- backend/index.js
+++ backend/index.js
@@ -7,23 +7,40 @@ const User = require("./models/user.model")
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect("mongodb://localhost:27017/mulingua-DB")
+mongoose.connect('mongodb://localhost:27017',
+  (err) => {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log("connected to database")
+    }
+  })
 
 app.post("/api/register", async (req, res) => {
   try {
-    const user = await User.create({
+    await User.create({
       name: req.body.name,
       email: req.body.email,
       age: req.body.age,
       address: req.body.address,
       password: req.body.password
     })
-    res.send({ status: 'ok' })
-  } catch {
+    res.json({ status: 'ok' })
+  } catch (error) {
+    console.log(error)
     res.json({ status: "error", error: "Duplicate email" })
   }
-  console.log(req.body)
-  res.json({ status: 'ok' })
+})
+app.post("/api/login", async (req, res) => {
+  const user = await User.findOne({
+    email: req.body.email,
+    password: req.body.password
+  })
+  if (user) {
+    return res.json({ status: "ok", user: true })
+  } else {
+    return res.json({ status: "error", user: false })
+  }
 })
 
 app.listen(5000, () => console.log("your port is working on 5000"))
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -4,8 +4,8 @@ const User = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   age: { type: Number, required: true },
   address: { type: String, required: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
 }, { collation: "user-data" })
 
 const model = mongoose.model("UserData", User)
-model.exports = model
+module.exports = model
