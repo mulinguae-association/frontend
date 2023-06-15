de149d8b6404c3187e5cdfccff9eb6a25318c014
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2023-06-15 04:22:21 +0300

    add Language switcher component

--- src/components/LanguageSwitcher.js
+++ src/components/LanguageSwitcher.js
@@ -1,24 +1,40 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import CustomDropdown from './Navbar/customDropdown/CustomDropDown';
+import useDirectionChange from './useDirectionChange';
 
-function LanguageSwitcher() {
+function LanguageSwitcher({ className }) {
   const { i18n } = useTranslation();
+  useDirectionChange(i18n.language)
   const [isLoading, setIsLoading] = useState(false);
 
-  const changeLanguage = (e) => {
+  const options = [
+    { label: 'English', value: 'En' },
+    { label: 'Spanish', value: 'Es' },
+    { label: 'French', value: 'Fr' },
+    { label: 'portuguese', value: 'Pt' },
+    { label: 'Arabic', value: 'Ar' },
+    { label: 'Quechua', value: 'Qu' },
+    { label: 'Kreol Morisyen', value: 'KM' },
+  ];
+  const handleSelect = (option) => {
     setIsLoading(true)
-    i18n.changeLanguage(e.target.value, () => setTimeout(() => setIsLoading(false), 1000));
+    localStorage.setItem('selectedLanguage', option.value); // Save selected language to local storage
+    i18n.changeLanguage(option.value, () => setTimeout(() => setIsLoading(false), 1000));
   };
+  // get language value from local storage
+  useEffect(() => {
+    const selectedLanguage = localStorage.getItem('selectedLanguage');
+    if (selectedLanguage) {
+      i18n.changeLanguage(selectedLanguage);
+    }
+  }, [i18n]);
 
   return (
     <>
       {isLoading && <div style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%,-50%)" }}>Loading...</div>}
-      <select className="language-select" onChange={changeLanguage}>
-        <option value="en">English</option>
-        <option value="fr">French</option>
-        <option value="ch">Chinese</option>
-        <option value="pr">portuguese</option>
-      </select>
+      <CustomDropdown className={className} options={options} onSelect={handleSelect} />
+
     </>
   );
 }
