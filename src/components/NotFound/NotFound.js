82cd735e815c8efec7fa2db78e49a67d69a1a58e
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2023-06-20 05:47:51 +0300

    add not found (404) page

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./NotFound.scss"
+import { useTranslation } from 'react-i18next';
+const NotFound = () => {
+  const { t, i18n } = useTranslation("notFound")
+  const checkLang = i18n.language === "Ar"
+  console.log(checkLang)
+  return (
+    <div className='not-found'>
+      <div className='container'>
+        <div className='content'>
+          <div className="crescent"></div>
+          <h1 className='text-styled'>404</h1>
+          <p style={checkLang ? { letterSpacing: "0.5rem" } : {}}> {t("not_found")}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default NotFound
