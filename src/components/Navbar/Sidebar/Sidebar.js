07f287c296b7b8b856efc045bd65a2b93a218603
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2023-06-25 07:52:19 +0300

    prevent third link from disapear on click & restyle sidebar

--- src/components/Navbar/Sidebar/Sidebar.js
+++ src/components/Navbar/Sidebar/Sidebar.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import "./Sidebar.scss"
 import LanguageSwitcher from '../../LanguageSwitcher'
-import NavLinks from '../NavLinks'
+import NavLinks from '../NavLinks/NavLinks'
 const Sidebar = (props) => {
   const handleEvent = (e) => {
+    const sideLinks = document.querySelector(".sideLinks")
+    const allLinks = sideLinks.querySelectorAll("a")
+    const pagseLink = allLinks[2]
+
     const target = e.target
     if (target.tagName === "SPAN" || target.tagName === "A" || target.tagName === "BUTTON") {
       props.setMenuOpen(false)
+      if (target === pagseLink) {
+        props.setMenuOpen(true)
+
+      }
     }
   }
   return (
