46d0ac781d9ea0c748ca1d8f96450209788615bc
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2023-06-25 07:24:57 +0300

    add its own translation & change background color

--- src/components/HeaderPages/Header.js
+++ src/components/HeaderPages/Header.js
@@ -1,17 +1,17 @@
-import React from 'react';
 import "./Header.scss";
 import { Link } from 'react-router-dom'
-const Header = ({ t, pageName }) => {
-
+import { useTranslation } from 'react-i18next';
+const Header = ({ pageName }) => {
+  const { t } = useTranslation("header", { ns: "header" });
   return (
-    <div className='header_pages change_direction'>
+    <div className='header_pages '>
       <div className='container'>
         <div className='wrapper_container'>
-          <ol className="breadcrumb">
+          <ul className="breadcrumb">
             <li className="breadcrumb_item"><Link to="/">{t("homeLink")}</Link></li>
             <li className='breed_slash'>/</li>
             <li className="breadcrumb_item" aria-current="page">{pageName}</li>
-          </ol>
+          </ul>
           <div className='content'>
             <div className='breed_crumb'>
               <h1>ACS Mulingua</h1>
