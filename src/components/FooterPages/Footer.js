5aa8807819a98854e8c77ddcb3c8d54d9a2216bb
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2023-06-25 07:19:41 +0300

    add className for links, translate it and change background

--- src/components/FooterPages/Footer.js
+++ src/components/FooterPages/Footer.js
@@ -13,28 +13,28 @@ const Footer = () => {
               <div className='col'>
                 <h3 className='footer_head'>{t("LearnM_title")}</h3>
                 <ul className="list_item">
-                  <li className='item'><Link className='link_item' to="">About Us</Link></li>
-                  <li className='item'><Link className='link_item' to="">Our classes</Link></li>
-                  <li className='item'><Link className='link_item' to="">FAQ</Link></li>
-                  <li className='item'><Link className='link_item' to="">Contact US</Link></li>
-                  <li className='item'><Link className='link_item' to="">Site Map</Link></li>
+                  <li className='item'><Link className='link_item' to="/about">{t("link")}</Link></li>
+                  <li className='item'><Link className='link_item' to="">{t("link1")}</Link></li>
+                  <li className='item'><Link className='link_item' to="">{t("link2")}</Link></li>
+                  <li className='item'><Link className='link_item' to="/contact">{t("link3")}</Link></li>
+                  <li className='item'><Link className='link_item' to="">{t("link4")}</Link></li>
                 </ul>
               </div>
               <div className='col'>
                 <h3 className='footer_head'>{t("ProudlyS_title")}</h3>
                 <ul className="list_item">
-                  <li className='item'><Link className='link_item' to="">Education for all</Link></li>
-                  <li className='item'><Link className='link_item' to="">Students of all ages</Link></li>
-                  <li className='item'><Link className='link_item' to="">Libraries</Link></li>
-                  <li className='item'><Link className='link_item' to="">Tutors Benefits</Link></li>
-                  <li className='item'><Link className='link_item' to="">Unity & solidarity</Link></li>
+                  <li className='item'><Link className='link_item' to="">{t("link5")}</Link></li>
+                  <li className='item'><Link className='link_item' to="">{t("link6")}</Link></li>
+                  <li className='item'><Link className='link_item' to="">{t("link7")}</Link></li>
+                  <li className='item'><Link className='link_item' to="">{t("link8")}</Link></li>
+                  <li className='item'><Link className='link_item' to="">{t("link9")}</Link></li>
                 </ul>
               </div>
               <div className='col'>
                 <h3 className='footer_head'>{t("JoinUs_title")}</h3>
                 <ul className="list_item">
-                  <li className='item'><Link className='link_item' to="">Work with Us</Link></li>
-                  <li className='item'><Link className='link_item' to="">Become a Tutor</Link></li>
+                  <li className='item'><Link className='link_item' to="">{t("link10")}</Link></li>
+                  <li className='item'><Link className='link_item' to="">{t("link11")}</Link></li>
                 </ul>
               </div>
             </div>
@@ -46,21 +46,21 @@ const Footer = () => {
           <div className='content'>
             <div className='info'>
               <p>
-                <span><Link to="">Privacy Policy</Link></span>
+                <span><Link to="">{t("link12")}</Link></span>
                 <span>|</span>
-                <span><Link to="">Privacy Notice</Link></span>
+                <span><Link to="">{t("link13")}</Link></span>
                 <span>|</span>
-                <span><Link to="">Terms of Use</Link></span>
+                <span><Link to="">{t("link14")}</Link></span>
               </p>
-              <p>©2023 ACS Mulingua.com </p>
-              <p>ACS Mulingua.com is controlled by ACS, a non profit association
-                situated in Lima, Peru.</p>
+              <p>{t("copyRight")}</p>
+              <p>{t("copyRight2")}
+              </p>
             </div>
             <div className='social_links'>
-              <img className="icon" src='images/icons/facebook.png' width="40" height="40" alt='facebook' />
-              <img className="icon" src='images/icons/twitter.png' width="40" height="40" alt='twitter' />
-              <img className="icon" src='images/icons/linkedIn.png' width="40" height="40" alt='linkedIn' />
-              <img className="icon" src='images/icons/instagram.png' width="40" height="40" alt=' instgram' />
+              <img className="icon" src={process.env.PUBLIC_URL + '/images/icons/facebook.png'} width="40" height="40" alt='facebook' />
+              <img className="icon" src={process.env.PUBLIC_URL + '/images/icons/twitter.png'} width="40" height="40" alt='twitter' />
+              <img className="icon" src={process.env.PUBLIC_URL + '/images/icons/linkedIn.png'} width="40" height="40" alt='linkedIn' />
+              <img className="icon" src={process.env.PUBLIC_URL + '/images/icons/instagram.png'} width="40" height="40" alt=' instgram' />
             </div>
           </div>
         </div>
