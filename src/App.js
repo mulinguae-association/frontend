4242058d729e6f98f7e4fde50b1adc0c5484fc8f
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2023-06-25 07:55:27 +0300

    add img animation and dynamic route & style ul,ol

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import './App.scss';
 import { Routes, Route } from "react-router-dom"
 import Home from './components/HomePage/Home';
@@ -8,16 +9,23 @@ import Loader from './components/Loader/Loader'
 import useLoader from './components/Loader/useLoader';
 import NOtFound from "./components/NotFound/NotFound"
 import ToTopBtn from "./components/ToTopBtn/ToTopBtn";
+import PagesDetails from './components/Navbar/NavLinks/NestedNavLinks/PagesDetails';
+import { useEffect, useState } from 'react';
 function App() {
   const isLoading = useLoader()
+  const [imgAnimation, setImgAnimation] = useState(false);
+  useEffect(() => {
+    setImgAnimation(true);
+  }, []);
   return (
     <div className="App">
       <Navbar />
       {isLoading ? <Loader /> :
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route exact path="/" element={<Home imgAnimation={imgAnimation} />} />
           <Route exact path='/About' element={<About />} />
           <Route exact path='/Register' element={<Register />} />
+          <Route path='/pages/:pageId' element={<PagesDetails />} />
           <Route path='*' element={<NOtFound />} />
         </Routes>
       }
@@ -27,3 +35,5 @@ function App() {
 }
 
 export default App;
+
+
