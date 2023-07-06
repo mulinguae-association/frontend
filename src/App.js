1979c7fae1a1a9a2e1a416a9b5b98bbd47302aa3
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2023-07-06 15:11:16 +0300

    add pages as dynamic route & LazyLoad for pages

--- src/App.js
+++ src/App.js
@@ -1,39 +1,46 @@
-
-import './App.scss';
-import { Routes, Route } from "react-router-dom"
-import Home from './components/HomePage/Home';
-import About from './components/AboutPage/About';
-import Register from './components/RegisterPage/Register';
-import Navbar from './components/Navbar/Navbar';
-import Loader from './components/Loader/Loader'
-import useLoader from './components/Loader/useLoader';
-import NOtFound from "./components/NotFound/NotFound"
+import "./App.scss";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/HomePage/Home";
+import Register from "./components/RegisterPage/Register";
+import Navbar from "./components/Navbar/Navbar";
+import Loader from "./components/Loader/Loader";
+import useLoader from "./components/Loader/useLoader";
+import NOtFound from "./components/NotFound/NotFound";
 import ToTopBtn from "./components/ToTopBtn/ToTopBtn";
-import PagesDetails from './components/Navbar/NavLinks/NestedNavLinks/PagesDetails';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 function App() {
-  const isLoading = useLoader()
-  const [imgAnimation, setImgAnimation] = useState(false);
-  useEffect(() => {
-    setImgAnimation(true);
-  }, []);
-  return (
-    <div className="App">
-      <Navbar />
-      {isLoading ? <Loader /> :
-        <Routes>
-          <Route exact path="/" element={<Home imgAnimation={imgAnimation} />} />
-          <Route exact path='/About' element={<About />} />
-          <Route exact path='/Register' element={<Register />} />
-          <Route path='/pages/:pageId' element={<PagesDetails />} />
-          <Route path='*' element={<NOtFound />} />
-        </Routes>
-      }
-      <ToTopBtn />
-    </div>
-  );
+	const isLoading = useLoader();
+	const [imgAnimation, setImgAnimation] = useState(false);
+	const LazyAbout = React.lazy(() => import("./components/AboutPage/About"));
+	const LazyPagesDetails = React.lazy(() =>
+		import("./components/Navbar/NavLinks/NestedNavLinks/PagesDetails")
+	);
+	useEffect(() => {
+		setImgAnimation(true);
+	}, []);
+	return (
+		<div className='App'>
+			<Navbar />
+			{isLoading ? (
+				<Loader />
+			) : (
+				<React.Suspense fallback={<Loader />}>
+					<Routes>
+						<Route
+							exact
+							path='/'
+							element={<Home imgAnimation={imgAnimation} />}
+						/>
+						<Route exact path='/About' element={<LazyAbout />} />
+						<Route exact path='/Register' element={<Register />} />
+						<Route path='/pages/:pageId' element={<LazyPagesDetails />} />
+						<Route path='*' element={<NOtFound />} />
+					</Routes>
+				</React.Suspense>
+			)}
+			<ToTopBtn />
+		</div>
+	);
 }
 
 export default App;
-
-
