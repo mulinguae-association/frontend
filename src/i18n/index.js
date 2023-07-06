a5e5aeb462d3ba90059e308ac967c76096d5b11c
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2023-07-06 17:23:49 +0300

    add another path for translations and new pages

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,44 +1,54 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import Backend from 'i18next-http-backend';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import Backend from "i18next-http-backend";
 
 const customBackend = {
-  loadPath: function (lng, ns) {
-    const pagesPrefix = 'pages/';
-    const pagesFolder = ns.indexOf(pagesPrefix) === 0 ? 'pages/' : '';
+	loadPath: function (lng, ns) {
+		const pagesPrefix = "pages/";
+		const pagesFolder = ns.indexOf(pagesPrefix) === 0 ? "pages/" : "";
 
-    return `/locales/${lng}/${pagesFolder}${ns}.json`;
-  },
+		return `/locales/${lng}/${pagesFolder}${ns}.json`;
+	},
 };
 
-const otherMethods = ['cookie', 'localStorage'];
+const otherMethods = ["cookie", "localStorage"];
 
 i18n
-  .use(Backend)
-  .use(initReactI18next)
-  .init({
-    fallbackLng: 'en',
-    debug: false,
-    interpolation: {
-      escapeValue: false,
-    },
-    detection: {
-      order: ['path', ...otherMethods],
-      lookupFromPathIndex: 0,
-      checkWhitelist: true
-    },
-    suspense: false,
-    backend: customBackend,
-    ns: ['home', 'about', 'pages/multilingualism', "pages/pagesLinks", 'notFound', 'header', 'footer'], // Specify the namespaces
-    defaultNS: 'home', // Set the default namespace
-    parse: function (data) {
-      try {
-        return JSON.parse(data);
-      } catch (error) {
-        console.error('Error parsing translation file:', error);
-        return {}; // Return an empty object as a fallback
-      }
-    },
-  });
+	.use(Backend)
+	.use(initReactI18next)
+	.init({
+		fallbackLng: "en",
+		debug: false,
+		interpolation: {
+			escapeValue: false,
+		},
+		detection: {
+			order: ["path", ...otherMethods],
+			lookupFromPathIndex: 0,
+			checkWhitelist: true,
+		},
+		suspense: false,
+		backend: customBackend,
+		ns: [
+			"home",
+			"about",
+			"pages/multilingualism",
+			"pages/linguicide",
+			"pages/pagesLinks",
+			"notFound",
+			"header",
+			"footer",
+			"global",
+		], // Specify the namespaces
+		defaultNS: "home", // Set the default namespace
+		parse: function (data) {
+			try {
+				return JSON.parse(data);
+			} catch (error) {
+				console.error("Error parsing translation file:", error);
+				return {}; // Return an empty object as a fallback
+			}
+		},
+	});
 
 export default i18n;
