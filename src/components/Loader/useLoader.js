242543136b7e1a9cbfa08ddf423f116dbefe1c1e
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2023-07-06 17:20:02 +0300

    add useLoader function that runs on load

--- src/components/Loader/useLoader.js
+++ src/components/Loader/useLoader.js
@@ -1,23 +1,26 @@
-import { useState, useEffect } from 'react';
+import { useContext } from "react";
+import { useEffect } from "react";
+import { AppContext } from "../../AppContext";
 
 const useLoader = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    const handleLoad = () => {
-      setTimeout(() => {
-        setIsLoading(false);
-      }, 1000);
-    };
-    if (document.readyState === 'complete') {
-      handleLoad();
-    } else {
-      document.addEventListener('readystatechange', handleLoad);
-    }
+	const { isLoading, setIsLoading } = useContext(AppContext);
 
-    return () => {
-      document.removeEventListener('readystatechange', handleLoad);
-    };
-  }, []);
-  return isLoading;
+	useEffect(() => {
+		const handleLoad = () => {
+			setTimeout(() => {
+				setIsLoading(false);
+			}, 1000);
+		};
+		if (document.readyState === "complete") {
+			handleLoad();
+		} else {
+			document.addEventListener("readystatechange", handleLoad);
+		}
+
+		return () => {
+			document.removeEventListener("readystatechange", handleLoad);
+		};
+	}, []);
+	return isLoading;
 };
 export default useLoader;
