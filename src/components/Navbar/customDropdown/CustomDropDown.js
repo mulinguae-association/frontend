0a710fd603b3188e3c1045a43ab5e29c0330a50f
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2023-07-14 12:37:37 +0300

    change localstorage to cookies and update style

--- src/components/Navbar/customDropdown/CustomDropDown.js
+++ src/components/Navbar/customDropdown/CustomDropDown.js
@@ -1,66 +1,72 @@
-import React, { useEffect, useRef, useState } from 'react';
-import './CustomDropdown.scss';
+import React, { useEffect, useRef, useState } from "react";
+import "./CustomDropdown.scss";
 
-const CustomDropdown = ({ options, onSelect, className }) => {
-  const [selectedOption, setSelectedOption] = useState(options[0]);
-  const [isOpen, setIsOpen] = useState(false);
-  const dropdownRef = useRef(null);
+const CustomDropdown = ({ options, onSelect, className, cookies }) => {
+	const [selectedOption, setSelectedOption] = useState(options[0]);
+	const [isOpen, setIsOpen] = useState(false);
+	const dropdownRef = useRef(null);
+	const handleOptionClick = (option) => {
+		setSelectedOption(option);
+		onSelect(option);
+		setIsOpen(false);
+	};
 
-  const handleOptionClick = (option) => {
-    setSelectedOption(option);
-    onSelect(option);
-    setIsOpen(false);
-    // Save selected language to local storage
-    localStorage.setItem('selectedLanguage', option.value);
-  };
+	const handleClickOutside = (event) => {
+		if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+			setIsOpen(false);
+		}
+	};
 
-  const handleClickOutside = (event) => {
-    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-      setIsOpen(false);
-    }
-  };
+	useEffect(() => {
+		document.addEventListener("click", handleClickOutside);
+		return () => {
+			document.removeEventListener("click", handleClickOutside);
+		};
+	}, []);
+	// change direction if the language was arabic
+	useEffect(() => {
+		if (cookies.selectedLanguage) {
+			const option = options.find((o) => o.value === cookies.selectedLanguage);
+			setSelectedOption(option);
+		}
+	}, [cookies, options]);
 
-  useEffect(() => {
-    document.addEventListener('click', handleClickOutside);
-    return () => {
-      document.removeEventListener('click', handleClickOutside);
-    };
-  }, []);
-  // change direction if the language was arabic
-  useEffect(() => {
-    const selectedLanguage = localStorage.getItem('selectedLanguage');
-    if (selectedLanguage) {
-      const option = options.find((o) => o.value === selectedLanguage);
-      setSelectedOption(option);
-    }
-  }, [options]);
+	return (
+		<div className={className} ref={dropdownRef}>
+			<div className='selected-option' onClick={() => setIsOpen(!isOpen)}>
+				{selectedOption
+					? `${selectedOption?.value === "Ar" ? "اللغات" : "languages"} (${
+							selectedOption?.value
+					  })`
+					: `languages (En) `}
+				<div>
+					<img
+						className='languages_icon'
+						src={"/images/icons/languages-icon.png"}
+						alt='languagesIcon'
+					/>
+					<span className={`arrow_down ${isOpen ? "rotate" : ""}`}></span>
+				</div>
+			</div>
+			{isOpen && (
+				<ul className='options thin-scroll'>
+					{options.map((option) => (
+						<li
+							key={option.value}
+							onClick={() => handleOptionClick(option)}
+							className={option === selectedOption ? "selected" : ""}>
+							<span className='option'>
+								{option.label}
 
-  return (
-    <div className={className} ref={dropdownRef}>
-      <div className="selected-option" onClick={() => setIsOpen(!isOpen)}>
-        {selectedOption ? `${selectedOption?.value === "Ar" ? "اللغات" : "languages"} (${selectedOption?.value})` : `languages (En) `}
-        <img className='languages_icon' src={process.env.PUBLIC_URL + '/images/icons/languages-icon.png'} alt='languagesIcon' />
-      </div>
-      {isOpen && (
-        <ul className="options thin-scroll">
-          {options.map((option) => (
-            <li
-              key={option.value}
-              onClick={() => handleOptionClick(option)}
-              className={option === selectedOption ? 'selected' : ''}
-            >
-              <span className='option'>
-                {option.label}
-
-                {selectedOption?.value === option?.value &&
-                  <img src={process.env.PUBLIC_URL + "/images/icons/right-icon.png"} alt="right-icon" />
-                }
-              </span>
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
-  );
+								{selectedOption?.value === option?.value && (
+									<img src={"/images/icons/right-icon.png"} alt='right-icon' />
+								)}
+							</span>
+						</li>
+					))}
+				</ul>
+			)}
+		</div>
+	);
 };
 export default CustomDropdown;
