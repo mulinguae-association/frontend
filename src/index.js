0d5eeef761676bef84acf9fa812590bfbffba7d6
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2023-07-14 11:49:25 +0300

    wrap the whole app with appContext

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { I18nextProvider } from 'react-i18next';
-import i18n from './i18n';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n";
+import { AppProvider } from "./AppContext";
+import { registerServiceWorker } from "./registerServiceWorker";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Call the registerServiceWorker function to register the service worker
+registerServiceWorker();
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <I18nextProvider i18n={i18n}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </I18nextProvider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<AppProvider>
+			<I18nextProvider i18n={i18n}>
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
+			</I18nextProvider>
+		</AppProvider>
+	</React.StrictMode>
 );
