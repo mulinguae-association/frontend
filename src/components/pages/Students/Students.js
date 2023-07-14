f9dd87ffce5af6d4559f908bfe04dd0424ea90ee
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2023-07-14 11:41:12 +0300

    add teachers & students components

--- src/components/pages/Students/Students.js
+++ src/components/pages/Students/Students.js
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useTranslation } from 'react-i18next';
+import PrintBtn from '../../PrintButton/PrintBtn';
 import "./Students.scss"
 const Students = () => {
+  const { t } = useTranslation('pages/students', { ns: 'students' });
+  console.log(t("page_title"))
+  const { t: global } = useTranslation('global', { ns: 'global' });
+  const componentRef = useRef();
   return (
-    <div>Students</div>
+    <main>
+      <div className='container'>
+        <section className='section multilingulism_info' ref={componentRef}>
+          <div className='print_btn'>
+            <PrintBtn print={global("print")} componentRef={componentRef} />
+          </div>
+          <h2 className='page_title'>{t("page_title")}</h2>
+          <p className='about'>
+            {t("sec1_about1")}
+            <span className='text-bold'>{t("sec1_about1_cont")}</span>
+          </p>
+          <p className='about'>{t("sec1_about2")}</p>
+          <p className='about'>{t("sec1_about3")}</p>
+          <p className='about'>{t("sec1_about4")}</p>
+          <p className='about'>{t("sec1_about5")}</p>
+          <p className='about'>{t("sec1_about6")}</p>
+          <p className='about'>{t("sec1_about7")}</p>
+          <p className='about'>{t("sec1_about8")}</p>
+          <p className='about'>
+            <a className='link' href={`${t("sec1_link1")}`}>
+              {t("sec1_link1")}
+            </a>
+          </p>
+          <p className='about'>
+            <a className='link' href={`${t("sec1_link2")}`}>
+              {t("sec1_link2")}
+            </a>
+          </p>
+          <p className='about'>
+            <a className='link' href={`${t("sec1_link3")}`}>
+              {t("sec1_link3")}
+            </a>
+          </p>
+          <p className='about'>
+            <a className='link' href={`${t("sec1_link4")}`}>
+              {t("sec1_link4")}
+            </a>
+          </p>
+        </section>
+      </div>
+    </main >
   )
 }
 
--- src/components/pages/Teachers/Teachers.js
+++ src/components/pages/Teachers/Teachers.js
@@ -1,8 +1,66 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import "./Teachers.scss"
+import { useTranslation } from 'react-i18next';
+import PrintBtn from '../../PrintButton/PrintBtn';
 const Teachers = () => {
+  const { t } = useTranslation('pages/teachers', { ns: 'teachers' });
+  console.log(t("page_title"))
+  const { t: global } = useTranslation('global', { ns: 'global' });
+  const componentRef = useRef();
   return (
-    <div>Teachers</div>
+    <main>
+      <div className='container'>
+        <section className='section multilingulism_info' ref={componentRef}>
+          <div className='print_btn'>
+            <PrintBtn print={global("print")} componentRef={componentRef} />
+          </div>
+          <h2 className='page_title'>{t("page_title")}</h2>
+          <p className='about'>{t("sec1_about1")}</p>
+          <h2 className='head'>{t("sec2_head")}</h2>
+          <p className='about'>{t("sec2_about1")}</p>
+          <p className='about'>{t("sec2_about2")}</p>
+          <p className='about'>{t("sec2_about3")}</p>
+          <h2 className='head'>{t("sec3_head")}</h2>
+          <p className='about'>{t("sec3_about1")}</p>
+          <p className='about'>{t("sec3_about2")}</p>
+          <p className='about'>{t("sec3_about3")}</p>
+          <p className='about'>{t("sec3_about4")}</p>
+          <h2 className='head'>{t("sec4_head")}</h2>
+          <p className='about'>{t("sec4_about1")}</p>
+          <p className='about'>{t("sec4_about2")}</p>
+          <p className='about'>{t("sec4_about3")}</p>
+          <h2 className='head'>{t("sec5_head")}</h2>
+          <p className='about'>{t("sec5_about1")}</p>
+          <p className='about'>{t("sec5_about2")}</p>
+          <p className='about'>
+            <a className='link' href={`${t("sec5_link1")}`}>
+              {t("sec5_link1")}
+            </a>
+          </p>
+          <p className='about'>
+            <a className='link' href={`${t("sec5_link2")}`}>
+              {t("sec5_link2")}
+            </a>
+          </p>
+          <p className='about'>
+            <a className='link' href={`${t("sec5_link3")}`}>
+              {t("sec5_link3")}
+            </a>
+          </p>
+          <p className='about'>
+            <a className='link' href={`${t("sec5_link4")}`}>
+              {t("sec5_link4")}
+            </a>
+          </p>
+          <p className='about'>
+            <a className='link' href={`${t("sec5_link5")}`}>
+              {t("sec5_link5")}
+            </a>
+          </p>
+          <p className='warning'>{t("warning")}</p>
+        </section>
+      </div>
+    </main >
   )
 }
 
