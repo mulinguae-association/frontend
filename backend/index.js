21d7a60dcd0cc8cbec57646750f48d6dc9aab2a1
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2023-08-17 07:13:26 +0300

    setup folder,files and packages

--- backend/index.js
+++ backend/index.js
@@ -1,46 +1,26 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const mongoose = require("mongoose")
-const User = require("./models/user.model")
-
+const bodyParser = require("body-parser")
+const routes = require("./routes")
+const { connectToDatabase } = require('./db/db');
+const dotenv = require("dotenv");
+dotenv.config();
+// use cors
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect('mongodb://localhost:27017',
-  (err) => {
-    if (err) {
-      console.log(err)
-    } else {
-      console.log("connected to database")
-    }
+app.use(bodyParser.json())
+// Connect to MongoDB
+connectToDatabase()
+  .then(() => {
+    console.log("Connected to MongoDB");
   })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
 
-app.post("/api/register", async (req, res) => {
-  try {
-    await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      age: req.body.age,
-      address: req.body.address,
-      password: req.body.password
-    })
-    res.json({ status: 'ok' })
-  } catch (error) {
-    console.log(error)
-    res.json({ status: "error", error: "Duplicate email" })
-  }
-})
-app.post("/api/login", async (req, res) => {
-  const user = await User.findOne({
-    email: req.body.email,
-    password: req.body.password
-  })
-  if (user) {
-    return res.json({ status: "ok", user: true })
-  } else {
-    return res.json({ status: "error", user: false })
-  }
-})
-
+// Mount the routes
+app.use("/api", routes);
+// start the server
 app.listen(5000, () => console.log("your port is working on 5000"))
