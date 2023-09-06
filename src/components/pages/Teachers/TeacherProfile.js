a15eb6fa78d351e557065841db758989b523932e
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2023-09-07 01:36:25 +0300

    add TeachersOverview section & Teacher Profile page with their style

--- src/components/pages/Teachers/TeacherProfile.js
+++ src/components/pages/Teachers/TeacherProfile.js
@@ -1,39 +1,122 @@
 // Any component that needs to use teachers' data
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import getTeachersData from '../../../asset.json'; // Import the function
+import { React, useContext, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { AppContext } from '../../../contexts/AppContext';
 
 const TeacherProfile = () => {
+  const { teachers, getTeachers } = useContext(AppContext)
+  const navigate = useNavigate()
+
   const { teacherId } = useParams(); // Get the teacherId from the URL parameter
-  console.log(teacherId)
-
-  const teachersData = getTeachersData // Use the function to get the data
-
+  console.log(teachers)
   // Find the teacher with matching teacherId
-  const teacher = teachersData.find(teacher => teacher.id === teacherId);
+  const teacher = teachers.find(teacher => teacher.firstName + teacher._id === teacherId);
+  useEffect(() => {
+    getTeachers()
+  }, [])
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+
+  const goBack = () => {
+    navigate(-1)
+    // Scroll to the specific section when the component mounts
+    setTimeout(() => {
+      const specificSection = document.querySelector('.teachers_overview');
+      if (specificSection) {
+        specificSection.scrollIntoView({ behavior: 'smooth' });
+      }
+    }, 100);
+  }
 
   if (!teacher) {
     return <div>Teacher not found</div>;
   }
-
   return (
-    <div>
-      <h2>{teacher.name}</h2>
-      <img src={teacher.image} alt={teacher.name} />
-      <p>{teacher.profile.bio}</p>
-      <h3>Subjects Taught:</h3>
-      <ul>
-        {teacher.profile.subjects.map((subject, index) => (
-          <li key={index}>{subject}</li>
-        ))}
-      </ul>
-      {teacher.customInfo &&
-        <ul>
-          <li>{teacher.customInfo?.contact}</li>
-          <li>{teacher.customInfo?.about}</li>
-        </ul>
-      }
-    </div >
+    <main className='page_content teacher_profile'>
+      <div className='container'>
+        <button className='btn back_btn' onClick={goBack}><span className='back_icon'><img width={25} height={25} src='/images/icons/backArrow.png' alt='back arrow' /></span> Back</button>
+        <section className='teacher_content'>
+          <div className='main_info'>
+            <div className='info'>
+              <h3>firstName: <span>{teacher.firstName}</span></h3>
+              <h3>lastName: <span>{teacher.lastName}</span></h3>
+              <h3>email: <span>{teacher.email}</span></h3>
+              <h3>Telephone/ Cellphone: <span>{teacher.telephone}</span></h3>
+            </div>
+            <img width="300px" height="300px" src={teacher?.image} alt={teacher?.name} onError={(e) => {
+              e.target.src = "/images/fallBackUser.png";
+            }} />
+          </div>
+          <div className='secondary_info'>
+            <div className='block'>
+              <h3>Teaching philosophy</h3>
+              {teacher?.teaching_philosophy ? (
+                <p>{teacher.teaching_philosophy}</p>
+              ) : (
+                <p>No teaching philosophy available</p>
+              )}
+            </div>
+            <div className='block'>
+              <h3>career summary</h3>
+              {teacher?.career_summary ? (
+                <p>{teacher.career_summary}</p>
+              ) : (
+                <p>No career summary available</p>
+              )}
+            </div>
+            <div className='block'>
+              <h3>Teaching methods and strategies</h3>
+              {teacher?.teaching_methods ? (
+                <p>{teacher.teaching_methods}</p>
+              ) : (
+                <p>No teaching methods and strategies available</p>
+              )}
+            </div>
+            <div className='block'>
+              <h3>Qualification and Certificates</h3>
+              {teacher?.qualification_cert ? (
+                <p>{teacher.qualification_cert}</p>
+              ) : (
+                <p>No qualification and certificates available</p>
+              )}
+            </div>
+            <div className='block'>
+              <h3>Teacher Collaboration</h3>
+              {teacher?.teacher_collaboration ? (
+                <p>{teacher.teacher_collaboration}</p>
+              ) : (
+                <p>No teacher collaboration information available</p>
+              )}
+            </div>
+            <div className='block'>
+              <h3>Classroom management</h3>
+              {teacher?.classroom_management ? (
+                <p>{teacher.classroom_management}</p>
+              ) : (
+                <p>No classroom management information available</p>
+              )}
+            </div>
+            <div className='block'>
+              <h3>Behavior management</h3>
+              {teacher?.behavior_management ? (
+                <p>{teacher.behavior_management}</p>
+              ) : (
+                <p>No behavior management information available</p>
+              )}
+            </div>
+            <div className='block'>
+              <h3>Additional Information</h3>
+              {teacher?.additional_info ? (
+                <p>{teacher.additional_info}</p>
+              ) : (
+                <p>No additional information available</p>
+              )}
+            </div>
+          </div>
+        </section>
+      </div>
+    </main>
   );
 };
 
--- src/components/pages/Teachers/TeachersOverview.js
+++ src/components/pages/Teachers/TeachersOverview.js
@@ -1,19 +1,60 @@
-import React from 'react'
+import { React, useContext, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Autoplay } from "swiper/modules";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import 'swiper/css';
 import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { deleteTeacher } from '../../../utils/apiUtility';
+import { AppContext } from '../../../contexts/AppContext';
+import EditTeacherForm from '../../Dashboard/EditTeacherForm';
+import { notifyError, notifySuccess } from '../../Notify';
 
-const TeachersOverview = ({ getTeachersData }) => {
+const TeachersOverview = ({ t }) => {
+  const { teachers, setTeachers, getTeachers } = useContext(AppContext)
+  const [editingTeacher, setEditingTeacher] = useState(null);
+
+  useEffect(() => {
+    getTeachers()
+  }, []);
+
+  const handleEditTeacher = (teacher) => {
+    if (editingTeacher && editingTeacher._id === teacher._id) {
+      // If the same teacher is being edited again, close the edit form
+      setEditingTeacher(null);
+    } else {
+      // Otherwise, open the edit form for the selected teacher
+      setEditingTeacher(teacher);
+    }
+  };
+  const handleUpdateTeacher = (updatedTeacher) => {
+    const updatedTeachers = teachers.map((teacher) =>
+      teacher._id === updatedTeacher._id ? updatedTeacher : teacher
+    );
+    setTeachers(updatedTeachers);
+    getTeachers()
+    // Clear the editingTeacher state to exit edit mode
+    setEditingTeacher(null);
+  }
+
+  const handleDeleteTeacher = async (teacherId) => {
+    try {
+      const res = await deleteTeacher(teacherId)
+      console.log(res)
+      res.success ? notifySuccess("successfuly deleted teacher") : notifyError("Faild deleted teacher")
+      getTeachers()
+    } catch (err) {
+      console.log(err)
+    }
+  }
   return (
     <Swiper
       style={{ width: "90%" }}
-      spaceBetween={10}
       slidesPerView={3}
       grabCursor={true}
       navigation
-      modules={[Navigation, Autoplay]}
+      pagination
+      modules={[Navigation, Pagination, Autoplay]}
       breakpoints={{
         0: {
           slidesPerView: 1,
@@ -21,11 +62,11 @@ const TeachersOverview = ({ getTeachersData }) => {
         },
         768: {
           slidesPerView: 2,
-          spaceBetween: 40,
+          spaceBetween: 30,
         },
         1024: {
           slidesPerView: 3,
-          spaceBetween: 50,
+          spaceBetween: 30,
         },
       }}
       autoplay={{
@@ -33,19 +74,32 @@ const TeachersOverview = ({ getTeachersData }) => {
         disableOnInteraction: true,
       }}
     >
-      {getTeachersData.map(teacher => (
-        <SwiperSlide tag='ul' key={teacher.id} >
+      {teachers?.map(teacher => (
+        <SwiperSlide tag='ul' key={teacher._id}>
           <li className='teacher_card'>
             <div className='teacher_content'>
-              <div className='imageContainer'>
-                <img width="500px" height="500px" className='teacher_image' src={`${teacher.img || "/images/user.png"}`} alt='teacher_image' />
-              </div>
-              <div className='teacher_group'>
-                <h4 className='teacher_name'>{teacher?.name}</h4>
-                <span className='teacher_job'>{teacher?.job}</span>
-              </div>
-              <p className='teacher_brief'>{teacher?.brief}</p>
-              <Link className='teacher_link' to={`/pages/Teachers/${teacher.id}`}>View Profile</Link>
+              {!editingTeacher || editingTeacher._id !== teacher._id ?
+                <>
+                  <div className='imageContainer'>
+                    <img width="500px" height="500px" className='teacher_image' src={teacher.image ? `${teacher.image}` : "/images/fallBackUser.png"} alt='teacher_image'
+                      onError={(e) => {
+                        e.target.src = "/images/fallBackUser.png";
+                      }}
+                    />
+                  </div>
+                  <div className='teacher_group'>
+                    <h4 className='teacher_name'>{teacher?.firstName} {teacher?.lastName}</h4>
+                    <span className='teacher_job'>{teacher?.jobBrief}</span>
+                  </div>
+                  <p className='teacher_brief scroll'>{teacher?.aboutTeacher}</p>
+                </>
+                : <EditTeacherForm teacher={editingTeacher} onUpdate={handleUpdateTeacher} />
+              }
+              <Link className='teacher_link' to={`/pages/Teachers/${teacher.firstName}${teacher._id}`}>{t("sec6_link1")}</Link>
+              <button className='deleteBtn' onClick={() => handleDeleteTeacher(teacher._id)}>x</button>
+              <button className='editBtn' onClick={(e) => handleEditTeacher(teacher)}>
+                <img height={"64px"} src='/images/icons/edit.png' alt='edit teacher card' />
+              </button>
             </div>
           </li>
         </SwiperSlide>
