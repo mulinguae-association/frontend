0e0d0c322c5c176c38d47df55839c97585f1ef04
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2024-07-03 17:55:20 +0300

    remove loader and add useCallback and useMemo functions to memorize the language

--- src/components/LanguageSwitcher.js
+++ src/components/LanguageSwitcher.js
@@ -1,23 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import CustomDropdown from './Navbar/customDropdown/CustomDropDown';
-import Loader from './Loader';
 import { useCookies } from 'react-cookie';
-import { AppContext } from '../contexts/AppContext';
-import { useContext } from 'react';
 import useDirectionChange from "../utils/useDirectionChange"
 import { useNavigate, useLocation } from 'react-router-dom';
 
 function LanguageSwitcher({ className }) {
   const { i18n } = useTranslation();
-  const { isLoading, setIsLoading } = useContext(AppContext);
-  const [cookies, setCookie] = useCookies(['selectedLanguage'])
+  const [cookies, setCookie] = useCookies(['selectedLanguage']);
   const navigate = useNavigate();
-  const location = useLocation()
+  const location = useLocation();
 
-  const options = [
-    { label: 'Arabic', value: 'Ar' },
+  const options = useMemo(() => [
     { label: 'English', value: 'En' },
+    { label: 'Arabic', value: 'Ar' },
     { label: 'French', value: 'Fr' },
     { label: 'Hindi', value: 'Hi' },
     { label: 'Kreol Morisyen', value: 'KM' },
@@ -27,19 +23,18 @@ function LanguageSwitcher({ className }) {
     { label: 'Russian', value: 'Ru' },
     { label: 'Spanish', value: 'Es' },
     { label: 'Urdu', value: 'Ur' },
+  ], []);
 
-  ];
-  const handleSelect = (option) => {
-    setIsLoading(true)
-    setCookie('selectedLanguage', option.value); // Save selected language to cookies
-    i18n.changeLanguage(option.value, () => setTimeout(() => setIsLoading(false), 400));
+  const handleSelect = useCallback((option) => {
+    setCookie('selectedLanguage', option.value, { path: '/' }); // Save selected language to cookies
     const currPath = window.location.pathname.split("/").slice(2).join("/");
     navigate(`/${option.value}/${currPath}`);
-  };
-  // Function to map URL language to option value
-  const mapUrlLanguageToOptionValue = (urlLanguage) => {
+    i18n.changeLanguage(option.value);
+  }, [i18n, navigate, setCookie]);
+
+  const mapUrlLanguageToOptionValue = useCallback((urlLanguage) => {
     return options.find((option) => option.value.toLowerCase() === urlLanguage.toLowerCase());
-  };
+  }, [options]);
 
   useEffect(() => {
     const urlLanguage = location.pathname.split("/")[1];
@@ -47,17 +42,18 @@ function LanguageSwitcher({ className }) {
     if (selectedOption) {
       handleSelect(selectedOption);
     }
-  }, []);
+  }, [location.pathname, handleSelect, mapUrlLanguageToOptionValue]);
 
-  useDirectionChange(i18n.language)
+  useDirectionChange(i18n.language);
+
   return (
     <>
-      {isLoading && <Loader />}
       <CustomDropdown
         className={className}
         cookies={cookies}
         options={options}
-        onSelect={handleSelect} />
+        onSelect={handleSelect}
+      />
     </>
   );
 }
