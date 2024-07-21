731182ad32c7d6be081a484b5b0ecc967c2ebc1e
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2024-07-21 22:52:17 +0300

    add waves animation for header & drop-shadow fot text and replace (flex-position

--- src/components/HeaderPages/index.js
+++ src/components/HeaderPages/index.js
@@ -20,6 +20,19 @@ const Header = ({ pageName }) => {
           </div>
         </div>
       </div>
+      <div className="custom-shape-divider-bottom-1707328329">
+        <svg className="waves" xmlns="http://www.w3.org/2000/svg" viewBox="0 24 150 28" preserveAspectRatio="none" shapeRendering="auto">
+          <defs>
+            <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+          </defs>
+          <g className="parallax">
+            <use href="#gentle-wave" x="48" y="0" fill="rgba(242, 245, 247,0.7)" />
+            <use href="#gentle-wave" x="48" y="3" fill="rgba(242, 245, 247,0.5)" />
+            <use href="#gentle-wave" x="48" y="5" fill="rgba(242, 245, 247,1)" />
+            <use href="#gentle-wave" x="48" y="7" fill="transparent" />
+          </g>
+        </svg>
+      </div>
     </div>
   )
 }
