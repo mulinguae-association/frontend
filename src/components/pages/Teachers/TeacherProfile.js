a508d3cbc5dba190b72dcf183362281ee428fc69
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2024-07-23 02:39:06 +0300

    add semantic tags & remove go back button cause not neccessary

--- src/components/pages/Teachers/TeacherProfile.js
+++ src/components/pages/Teachers/TeacherProfile.js
@@ -1,59 +1,51 @@
-// Any component that needs to use teachers' data
-import { React, useContext, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import React, { useContext, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import { AppContext } from '../../../contexts/AppContext';
-import "./Teachers.scss"
+import "./Teachers.scss";
 import { useTranslation } from 'react-i18next';
-import { BiSolidArrowToLeft } from 'react-icons/bi';
-import { IoIosArrowBack } from 'react-icons/io';
-const TeacherProfile = () => {
-  const { teachers, getTeachers } = useContext(AppContext)
-  const { t, i18n } = useTranslation('pages/teachers', { ns: 'teachers' });
-  const navigate = useNavigate()
-  const lang = i18n.language === "Ar"
-  const { teacherId } = useParams(); // Get the teacherId from the URL parameter
-  // Find the teacher with matching teacherId
-  const teacher = teachers.find(teacher => teacher.firstName + teacher._id === teacherId);
-  useEffect(() => {
-    getTeachers()
-  }, [])
-  useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
 
-  const goBack = () => {
-    navigate(-1)
-    // Scroll to the specific section when the component mounts
-    setTimeout(() => {
-      const specificSection = document.querySelector('.teachers_overview');
-      if (specificSection) {
-        specificSection.scrollIntoView({ behavior: 'smooth' });
-      }
-    }, 100);
-  }
+const TeacherProfile = () => {
+  const { teachers, getTeachers } = useContext(AppContext);
+  const { t } = useTranslation('pages/teachers');
+  const { teacherId } = useParams();
+
+  useEffect(() => {
+    getTeachers();
+  }, [getTeachers]);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  // Find the teacher with matching teacherId
+  const teacher = teachers.find(teacher => teacher.firstName + "_" + teacher._id === teacherId);
 
   if (!teacher) {
-    return <div>Teacher not found</div>;
+    return <div className='not-found'>Teacher not found</div>;
   }
+
   return (
     <main className='page_content teacher_profile'>
       <div className='container'>
-        <button className='btn back_btn' onClick={goBack}><span style={lang ? { rotate: "180deg" } : {}} className='back_icon'>
-          <IoIosArrowBack size={26} />
-        </span> {t('sec7_link1')}</button>
         <section className='teacher_content'>
-          <div className='main_info'>
+          <header className='main_info'>
             <div className='info'>
-              <h3>{t('sec7_name1')}: <span>{teacher.firstName}</span></h3>
-              <h3>{t('sec7_name2')}: <span>{teacher.lastName}</span></h3>
-              <h3>{t('sec7_email')}: <span>{teacher.email}</span></h3>
-              <h3>{t('sec7_phone')}: <span>{teacher.telephone}</span></h3>
+              <h3>{t('sec7_name1')}: <span>{teacher?.firstName}</span></h3>
+              <h3>{t('sec7_name2')}: <span>{teacher?.lastName}</span></h3>
+              <h3>{t('sec7_email')}: <span>{teacher?.email}</span></h3>
+              <h3>{t('sec7_phone')}: <span>{teacher?.telephone}</span></h3>
             </div>
-            <img width="300px" height="300px" src={teacher?.image} onError={(e) => {
-              e.target.src = "/images/fallBackUser.png";
-            }} alt={teacher?.firstName + ' image'} />
-          </div>
-          <div className='secondary_info'>
+            <img
+              width="300px"
+              height="300px"
+              src={teacher?.image}
+              onError={(e) => {
+                e.target.src = "/images/fallBackUser.png";
+              }}
+              alt={teacher?.firstName + ' image'}
+            />
+          </header>
+          <article className='secondary_info'>
             <div className='block'>
               <h3>{t('sec7_about1')}</h3>
               {teacher?.teaching_philosophy ? (
@@ -118,7 +110,7 @@ const TeacherProfile = () => {
                 <p>No {t('sec7_about8')} available</p>
               )}
             </div>
-          </div>
+          </article>
         </section>
       </div>
     </main>
