e4eabec6b755dc354b12614b42f17e834c1c2c17
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2024-07-23 04:17:43 +0300

    add semantic tags and style image & paragraphs

--- src/components/pages/Students/index.js
+++ src/components/pages/Students/index.js
@@ -1,56 +1,65 @@
-import React, { useRef } from 'react'
+import React, { useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import PrintBtn from '../../PrintButton';
-import "./Students.scss"
-import "../pagesStyle.scss"
+import "./Students.scss";
+import "../pagesStyle.scss";
+
 const Students = () => {
-  const { t } = useTranslation('pages/students', { ns: 'students' });
-  console.log(t("page_title"))
-  const { t: global } = useTranslation('global', { ns: 'global' });
+  const { t } = useTranslation('pages/students');
+  const { t: global } = useTranslation('global');
   const componentRef = useRef();
+
   return (
     <main>
       <div className='container'>
-        <section className='section multilingulism_info' ref={componentRef}>
+        <section className='section students multilingulism_info' ref={componentRef}>
           <div className='print_btn'>
             <PrintBtn print={global("print")} componentRef={componentRef} />
           </div>
-          <h2 className='page_title'>{t("page_title")}</h2>
-          <p className='about'>
-            {t("sec1_about1")}
-            <span className='text-bold'>{t("sec1_about1_cont")}</span>
-          </p>
-          <p className='about'>{t("sec1_about2")}</p>
-          <p className='about'>{t("sec1_about3")}</p>
-          <p className='about'>{t("sec1_about4")}</p>
-          <p className='about'>{t("sec1_about5")}</p>
-          <p className='about'>{t("sec1_about6")}</p>
-          <p className='about'>{t("sec1_about7")}</p>
-          <p className='about'>{t("sec1_about8")}</p>
-          <p className='about'>
-            <a className='link' href={`${t("sec1_link1")}`}>
-              {t("sec1_link1")}
-            </a>
-          </p>
-          <p className='about'>
-            <a className='link' href={`${t("sec1_link2")}`}>
-              {t("sec1_link2")}
-            </a>
-          </p>
-          <p className='about'>
-            <a className='link' href={`${t("sec1_link3")}`}>
-              {t("sec1_link3")}
-            </a>
-          </p>
-          <p className='about'>
-            <a className='link' href={`${t("sec1_link4")}`}>
-              {t("sec1_link4")}
-            </a>
-          </p>
+          <header>
+            <h2 className='page_title'>{t("page_title")}</h2>
+          </header>
+          <article className="group">
+            <div className="students_img">
+              <img src='/images/studentsImg.jpg' alt='students-img' />
+            </div>
+            <div>
+              <p>{t("sec1_about1")}</p>
+              <p>{t("sec1_about2")}</p>
+              <p>{t("sec1_about3")}</p>
+              <p>{t("sec1_about4")}</p>
+              <p>{t("sec1_about5")}</p>
+              <p>{t("sec1_about6")}</p>
+              <p>{t("sec1_about7")}</p>
+              <p>{t("sec1_about8")}</p>
+              <nav>
+                <p>
+                  <a className='link' href={`${t("sec1_link1")}`}>
+                    {t("sec1_link1")}
+                  </a>
+                </p>
+                <p>
+                  <a className='link' href={`${t("sec1_link2")}`}>
+                    {t("sec1_link2")}
+                  </a>
+                </p>
+                <p>
+                  <a className='link' href={`${t("sec1_link3")}`}>
+                    {t("sec1_link3")}
+                  </a>
+                </p>
+                <p>
+                  <a className='link' href={`${t("sec1_link4")}`}>
+                    {t("sec1_link4")}
+                  </a>
+                </p>
+              </nav>
+            </div>
+          </article>
         </section>
       </div>
-    </main >
-  )
+    </main>
+  );
 }
 
-export default Students
+export default Students;
