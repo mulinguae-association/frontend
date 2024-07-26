16369aa408646dba8f5a9d5c5d65ce2299c9fac3
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2024-07-26 06:45:57 +0300

    add check if user didn't change his info ? / make image-input empty after sent req 200 / add width&height for img / organize the file

--- src/components/AuthPages/UserSettings.js
+++ src/components/AuthPages/UserSettings.js
@@ -1,33 +1,29 @@
 import React, { useState } from 'react';
-import "./UserSettings.scss";
 import axios from 'axios';
 import { useAuth } from '../../contexts/AuthContext';
-import { notifyError, notifySuccess } from '../Notify';
-import Tooltip from '../HelperComponents/toolTip';
 import { useBlogPosts } from '../../contexts/BlogsContext';
 import { useGlobal } from '../../contexts/AppContext';
 import { useTranslation } from 'react-i18next';
+import Tooltip from '../HelperComponents/toolTip';
+import { notifyError, notifySuccess } from '../Notify';
+import "./UserSettings.scss";
 
 function UserSettings() {
+  const { t } = useTranslation("authPages/userProfile");
   const { userData, setUserData } = useAuth();
   const { fetchAcceptedData } = useBlogPosts();
-  const [name, setName] = useState(userData?.name);
-  const [email, setEmail] = useState(userData?.email);
-  const [profileImage, setProfileImage] = useState({});
   const { isBtnLoading, setButtonLoading } = useGlobal();
-  const { t } = useTranslation("authPages/userProfile")
-  const handleNameChange = (e) => {
-    setName(e.target.value);
-  };
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
+  const [name, setName] = useState(userData?.name || '');
+  const [email, setEmail] = useState(userData?.email || '');
+  const [profileImage, setProfileImage] = useState(null);
+
+  const handleNameChange = (e) => setName(e.target.value);
+  const handleEmailChange = (e) => setEmail(e.target.value);
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
     const maxImageSize = 1024 * 1024; // 1 MB
-
     if (file && file.size <= maxImageSize) {
       setProfileImage(file);
     } else {
@@ -38,37 +34,39 @@ function UserSettings() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    if (name === userData?.name && email === userData?.email && !profileImage) {
+      notifyError(t("noChangesMessage"));
+      return;
+    }
+
     const formData = new FormData();
     formData.append('name', name);
     formData.append('email', email);
     if (profileImage) {
-      formData.append('profileImage', profileImage); // Add the image file if it exists
+      formData.append('profileImage', profileImage);
     }
 
     try {
-      setButtonLoading("userSettingsBtn", true)
+      setButtonLoading("userSettingsBtn", true);
       const res = await axios.put('/api/auth/user/update', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data', // Set the correct content type for FormData
-        },
+        headers: { 'Content-Type': 'multipart/form-data' },
       });
+
       if (res.status === 200) {
-        console.log(res)
         notifySuccess(res.data.message);
         setUserData(res.data.data);
         fetchAcceptedData();
+        setProfileImage(null);
       } else {
-        console.log(res)
         notifyError(res.data.error);
       }
     } catch (error) {
-      notifyError(error.response.data.error);
-      console.error(error);
+      notifyError(error.response?.data?.error || error.message);
     } finally {
-      setButtonLoading("userSettingsBtn", false)
+      setButtonLoading("userSettingsBtn", false);
     }
   };
-  console.log(userData)
+
   return (
     <main className='settings_page'>
       <div className='container'>
@@ -84,9 +82,20 @@ function UserSettings() {
             <input type="email" value={email} onChange={handleEmailChange} />
           </div>
           <div className='input_container'>
-            <img src={userData?.profileImage ? userData?.profileImage : "/images/fallBackUser.png"} alt='personal profile' onError={(e) => e.target.src = "/images/fallBackUser.png"} />
+            <div className='img_container'>
+              <img
+                src={
+                  profileImage
+                    ? URL.createObjectURL(profileImage)
+                    : userData?.profileImage || "/images/fallBackUser.png"
+                }
+                width="100px"
+                height="100px"
+                alt='personal profile'
+                onError={(e) => e.target.src = "/images/fallBackUser.png"} />
+            </div>
             <label className="upload_image" htmlFor="image">
-              {!profileImage ? t("changeImageText") : profileImage?.name || t("changeImageText")}
+              {t("changeImageText")}
               <Tooltip text={t("chooseImageTooltip")} />
             </label>
             <input
@@ -97,7 +106,9 @@ function UserSettings() {
               onChange={handleImageChange}
             />
           </div>
-          <button disabled={isBtnLoading["userSettingsBtn"]} type="submit">{isBtnLoading["userSettingsBtn"] ? t("savingText") : t("saveChangesButton")}</button>
+          <button disabled={isBtnLoading["userSettingsBtn"]} type="submit">
+            {isBtnLoading["userSettingsBtn"] ? t("savingText") : t("saveChangesButton")}
+          </button>
         </form>
       </div>
     </main>
