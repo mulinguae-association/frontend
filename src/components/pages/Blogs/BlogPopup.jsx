1cd9ea09f4d9c3136607a5abf32f7c027e75c2aa
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2024-07-29 04:38:41 +0300

    add overlay to popup & blgo_content class

--- src/components/pages/Blogs/BlogPopup.jsx
+++ src/components/pages/Blogs/BlogPopup.jsx
@@ -22,80 +22,85 @@ const BlogPopup = ({
     useCommentEditState();
 
   return (
-    <div className="blog_popup">
-      {showFullContent && (
-        <>
-          <h1 className="blog_title">{blog.title}</h1>
-          <div dangerouslySetInnerHTML={{ __html: blog.content }} />
-        </>
-      )}
-      {showAllComments && (
-        <div className="popup_content">
-          <div className="comments_header">
-            <h1 className="comment_title">
-              <span className="special">{blog?.title} </span>
-              Comments
-            </h1>
-          </div>
-          {comments.map((comment) =>
-            comment?.status === "accepted" ? (
-              <div key={comment._id} className="comments_content">
-                <div className="comment">
-                  <div className="comments_content">
-                    {/* Map and display all comments */}
-                    <Comment
-                      key={comment._id}
-                      comment={comment}
-                      userData={userData}
-                      isEditComment={isEditComment}
-                      setIsEditComment={setIsEditComment}
-                      editCommentId={editCommentId}
-                      handleRemoveComment={handleRemoveComment}
-                      handleEdit={handleEdit}
-                      updateCommentLocally={updateCommentLocally}
-                      updateLike={updateLike}
+    <div className="blog_overlay">
+      <div className="blog_popup">
+        {showFullContent && (
+          <>
+            <h1 className="blog_title">{blog.title}</h1>
+            <div
+              className="blog_content"
+              dangerouslySetInnerHTML={{ __html: blog.content }}
+            />
+          </>
+        )}
+        {showAllComments && (
+          <div className="popup_content">
+            <div className="comments_header">
+              <h1 className="comment_title">
+                <span className="special">{blog?.title} </span>
+                Comments
+              </h1>
+            </div>
+            {comments.map((comment) =>
+              comment?.status === "accepted" ? (
+                <div key={comment._id} className="comments_content">
+                  <div className="comment">
+                    <div className="comments_content">
+                      {/* Map and display all comments */}
+                      <Comment
+                        key={comment._id}
+                        comment={comment}
+                        userData={userData}
+                        isEditComment={isEditComment}
+                        setIsEditComment={setIsEditComment}
+                        editCommentId={editCommentId}
+                        handleRemoveComment={handleRemoveComment}
+                        handleEdit={handleEdit}
+                        updateCommentLocally={updateCommentLocally}
+                        updateLike={updateLike}
+                        handleSubmit={handleSubmit}
+                      />
+                    </div>
+                    <div className="replies">
+                      {comment &&
+                        comment?.replies.map((reply) =>
+                          reply.status === "accepted" ? (
+                            <CommentReply
+                              key={reply._id}
+                              comment={reply}
+                              isEditComment={isEditComment}
+                              setIsEditComment={setIsEditComment}
+                              editCommentId={editCommentId}
+                              handleRemoveComment={handleRemoveComment}
+                              handleEdit={handleEdit}
+                              updateCommentLocally={updateCommentLocally}
+                              updateLike={updateLike}
+                              handleSubmit={handleSubmit}
+                            />
+                          ) : (
+                            ""
+                          )
+                        )}
+                    </div>
+                    <ReplyForm
+                      commentsId={comment?._id}
                       handleSubmit={handleSubmit}
                     />
                   </div>
-                  <div className="replies">
-                    {comment &&
-                      comment?.replies.map((reply) =>
-                        reply.status === "accepted" ? (
-                          <CommentReply
-                            key={reply._id}
-                            comment={reply}
-                            isEditComment={isEditComment}
-                            setIsEditComment={setIsEditComment}
-                            editCommentId={editCommentId}
-                            handleRemoveComment={handleRemoveComment}
-                            handleEdit={handleEdit}
-                            updateCommentLocally={updateCommentLocally}
-                            updateLike={updateLike}
-                            handleSubmit={handleSubmit}
-                          />
-                        ) : (
-                          ""
-                        )
-                      )}
-                  </div>
-                  <ReplyForm
-                    commentsId={comment?._id}
-                    handleSubmit={handleSubmit}
-                  />
                 </div>
-              </div>
-            ) : (
-              ""
-            )
-          )}
-          <button className="popup_close" onClick={() => show(false)}>
-            show less comments
-          </button>
-        </div>
-      )}
-      <button onClick={() => show(false)} className="blog_close">
-        x
-      </button>
+              ) : (
+                ""
+              )
+            )}
+            <button className="popup_close" onClick={() => show(false)}>
+              show less comments
+            </button>
+          </div>
+        )}
+        <button onClick={() => show(false)} className="blog_close">
+          x
+        </button>
+      </div>
     </div>
   );
 };
