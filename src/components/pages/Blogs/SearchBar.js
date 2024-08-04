d9d796f0aa1e7704cfc78e1dc17f3bb60120d5b4
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2024-08-04 07:25:52 +0300

    add loader above search-input and style it  - make reply form has direct button

--- src/components/pages/Blogs/SearchBar.js
+++ src/components/pages/Blogs/SearchBar.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import InputField from '../../HelperComponents/InputField'; // Import your InputField component
+import { useBlogPosts } from '../../../contexts/BlogsContext';
+import { BiLoaderAlt } from 'react-icons/bi';
 
 const SearchBar = (props) => {
+  const { isSearching } = useBlogPosts();
   return (
-    <InputField
-      className={`search ${props.className}`}
-      label="Search by title or user name"
-      id={props.id}
-      name={props.id}
-      type="search"
-      placeholder="Search by title or user name & click Enter"
-      onChange={props.handleSearchChange}
-      searchQuery={props.searchQuery}
-      onKeyDown={props.onKeyDown}
-    />
+    <div className={`search ${props.className}`}>
+      {isSearching ?
+        <div
+          className='loader-icon'>
+          <BiLoaderAlt />
+        </div>
+        : null
+      }
+      <InputField
+        className={`input-search`}
+        label="Search by title or user name"
+        id={props.id}
+        name={props.id}
+        type="search"
+        placeholder="Search by title or user name & click Enter"
+        onChange={props.handleSearchChange}
+        searchQuery={props.searchQuery}
+        onKeyDown={props.onKeyDown}
+      />
+    </div>
   );
 };
 
