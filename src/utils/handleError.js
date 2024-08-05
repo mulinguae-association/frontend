3199aee8a6fd40f52398bf466c312edb135e3077
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2024-08-05 19:36:35 +0300

    make masseage concise & return it without notify it

--- src/utils/handleError.js
+++ src/utils/handleError.js
@@ -1,10 +1,8 @@
-import { notifyError } from "../components/Notify";
-
 const handleError = (error) => {
-  let errorMessage = "An unexpected error occurred. Please try again later.";
+  let errorMessage = "An error occurred. Please try again later.";
 
   if (error.response) {
-    const { status } = error.response;
+    const { status } = error?.response;
     switch (status) {
       case 400:
         errorMessage = "Bad request. Please check your input and try again.";
@@ -13,29 +11,29 @@ const handleError = (error) => {
         errorMessage = "Unauthorized. Please log in and try again.";
         break;
       case 403:
-        errorMessage = "Forbidden. You do not have permission to access this resource.";
+        errorMessage = "Forbidden. You don't have permission to access this resource.";
         break;
       case 404:
-        errorMessage = "The requested resource could not be found. It may have been moved, deleted, or does not exist.";
+        errorMessage = "Resource not found. It may have been moved, deleted, or does not exist.";
         break;
       case 500:
-        errorMessage = "There was a problem with the server. Please try again later.";
+        errorMessage = "Server error. Please try again later.";
         break;
       case 502:
-        errorMessage = "Bad gateway. The server received an invalid response from the upstream server.";
+        errorMessage = "Bad gateway. The server received an invalid response.";
         break;
       case 503:
-        errorMessage = "Service unavailable. The server is currently unable to handle the request due to maintenance or overload.";
+        errorMessage = "Service unavailable. Please try again later.";
         break;
       case 504:
-        errorMessage = "Gateway timeout. The server did not receive a timely response from the upstream server.";
+        errorMessage = "Gateway timeout. Please try again later.";
         break;
       default:
-        errorMessage = "An unexpected error occurred. Please try again later.";
+        errorMessage = "An error occurred. Please try again later.";
     }
   }
 
-  notifyError(errorMessage);
+  return errorMessage;
 };
 
 export default handleError;
