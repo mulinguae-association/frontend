a296192df92072672ee6816ea6842ce9636850a3
abdelrhman-shibl44 <abdulrhman.mahmoud44@gmail.com>
2025-10-16 16:33:30 +0300

    style: Format code for consistency and readability in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import "./i18n";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
@@ -6,30 +7,30 @@ import { AppProvider } from "./contexts/AppContext";
 import { registerServiceWorker } from "./registerServiceWorker";
 import { AuthProvider } from "./contexts/AuthContext";
 import { BlogPostsProvider } from "./contexts/BlogsContext";
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from "react-query";
 
 // Call the registerServiceWorker function to register the service worker
 registerServiceWorker();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const queryClient = new QueryClient({
-	defaultOptions: {
-		queries: {
-			refetchOnWindowFocus: false,
-			retry: 1
-		}
-	}
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+  },
 });
 
 root.render(
-	<React.StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<AppProvider>
-				<BlogPostsProvider>
-					<AuthProvider>
-						<App />
-					</AuthProvider>
-				</BlogPostsProvider>
-			</AppProvider>
-		</QueryClientProvider>
-	</React.StrictMode>
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <AppProvider>
+        <BlogPostsProvider>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </BlogPostsProvider>
+      </AppProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
 );
