2c76b7e1a4621fd7134dc8467ff78a8e6415abbf
abdelrhman-shibl44 <abdulrhman.mahmoud44@gmail.com>
2026-01-18 05:56:34 +0200

    add pages/multilingualism to ns translation

--- src/components/pages/Multilingualism/index.jsx
+++ src/components/pages/Multilingualism/index.jsx
@@ -1,28 +1,30 @@
-import React, { useRef } from 'react';
-import './Multilingualism.scss';
-import '../pagesStyle.scss';
-import { useTranslation } from 'react-i18next';
-import PrintBtn from '../../PrintButton';
+import React, { useRef } from "react";
+import "./Multilingualism.scss";
+import "../pagesStyle.scss";
+import { useTranslation } from "react-i18next";
+import PrintBtn from "../../PrintButton";
 
 const Multilingualism = () => {
-  const { t } = useTranslation('pages/multilingualism', { ns: 'multilingualism' });
-  const langFactsArr = t('about_langFacts', { returnObjects: true });
-  const { t: global } = useTranslation('global', { ns: 'global' });
+  const { t } = useTranslation("pages/multilingualism", {
+    ns: "pages/multilingualism",
+  });
+  const langFactsArr = t("about_langFacts", { returnObjects: true });
+  const { t: global } = useTranslation("global", { ns: "global" });
   const componentRef = useRef();
 
   return (
     <main>
-      <div className='container'>
-        <article className='section multilingulism_info' ref={componentRef}>
-          <div className='print_btn'>
+      <div className="container">
+        <article className="section multilingulism_info" ref={componentRef}>
+          <div className="print_btn">
             <PrintBtn print={global("print")} componentRef={componentRef} />
           </div>
           <header>
-            <h1 className='page_title'>{t("page_title")}</h1>
+            <h1 className="page_title">{t("page_title")}</h1>
           </header>
           <section>
-            <h2 className='head'>{t("definition_head")}</h2>
-            <div className='about'>
+            <h2 className="head">{t("definition_head")}</h2>
+            <div className="about">
               <p>{t("about_definition")}</p>
               <p>{t("about_definition2")}</p>
               <p>{t("about_definition3")}</p>
@@ -31,46 +33,82 @@ const Multilingualism = () => {
             </div>
           </section>
           <section>
-            <h2 className='head'>{t("importanceLanguage_head")}</h2>
-            <div className='about'>
+            <h2 className="head">{t("importanceLanguage_head")}</h2>
+            <div className="about">
               <p>{t("about_importanceLang")}</p>
-              <p>{t("about_importanceLang2")}
-                <a className='link' href='https://www.un.org/en/about-us/universal-declaration-of-human-rights'>{t("link")}</a>
+              <p>
+                {t("about_importanceLang2")}
+                <a
+                  className="link"
+                  href="https://www.un.org/en/about-us/universal-declaration-of-human-rights"
+                >
+                  {t("link")}
+                </a>
                 {t("about_importanceLang3")}
-                <a className='link' href='https://www.unesco.org/en/legal-affairs/recommendation-concerning-promotion-and-use-multilingualism-and-universal-access-cyberspace?hub=66535'>{t("link2")}</a>
+                <a
+                  className="link"
+                  href="https://www.unesco.org/en/legal-affairs/recommendation-concerning-promotion-and-use-multilingualism-and-universal-access-cyberspace?hub=66535"
+                >
+                  {t("link2")}
+                </a>
                 {t("about_importanceLang4")}
               </p>
               <p>
                 {t("about_importanceLang5")}
-                <a className='link' href='https://documents-dds-ny.un.org/doc/UNDOC/GEN/N19/426/26/PDF/N1942626.pdf?OpenElement'>{t("link3")}</a>
+                <a
+                  className="link"
+                  href="https://documents-dds-ny.un.org/doc/UNDOC/GEN/N19/426/26/PDF/N1942626.pdf?OpenElement"
+                >
+                  {t("link3")}
+                </a>
               </p>
               <p>
                 {t("about_importanceLang6")}
-                <a className='link' href='https://idil2022-2032.org/'>{t("link4")}</a>
+                <a className="link" href="https://idil2022-2032.org/">
+                  {t("link4")}
+                </a>
                 {t("about_importanceLang7")}
               </p>
             </div>
           </section>
           <section>
-            <h2 className='head'>{t("linguaFranca_head")}</h2>
-            <div className='about'>
-              <p>{t("about_linguaFranca")}
-                <a className='link' href="https://www.un.org/fr/department-global-communications">{t("link5")}</a>
+            <h2 className="head">{t("linguaFranca_head")}</h2>
+            <div className="about">
+              <p>
+                {t("about_linguaFranca")}
+                <a
+                  className="link"
+                  href="https://www.un.org/fr/department-global-communications"
+                >
+                  {t("link5")}
+                </a>
                 {t("about_linguaFranca2")}
-                <a className='link' href="https://www.un.org/en/multilingualism-web-standards">{t("link6")}</a>
-                <a className='link' href="https://www.un.org/en/our-work/official-languages">{t("link7")}</a>
+                <a
+                  className="link"
+                  href="https://www.un.org/en/multilingualism-web-standards"
+                >
+                  {t("link6")}
+                </a>
+                <a
+                  className="link"
+                  href="https://www.un.org/en/our-work/official-languages"
+                >
+                  {t("link7")}
+                </a>
               </p>
             </div>
           </section>
-          <section className='languages_landscape'>
-            <h2 className='head'>{t("langFacts_head")}</h2>
-            <ol className='about'>
+          <section className="languages_landscape">
+            <h2 className="head">{t("langFacts_head")}</h2>
+            <ol className="about">
               {langFactsArr.map((fact, idx) => (
-                <li className='about' key={idx}>
+                <li className="about" key={idx}>
                   <span>{fact.title}</span>
                   <span>{t(fact?.title2)}</span>
                   <span>
-                    <a className='link' href={fact?.link}>{t(fact?.link)}</a>
+                    <a className="link" href={fact?.link}>
+                      {t(fact?.link)}
+                    </a>
                   </span>
                 </li>
               ))}
@@ -80,6 +118,6 @@ const Multilingualism = () => {
       </div>
     </main>
   );
-}
+};
 
 export default Multilingualism;
